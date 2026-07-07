90d6d021b4764460db178e20969fa95fd2c1d37d
phileory <muhammad.luthfi.chani@gmail.com>
2026-07-07 21:45:36 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ksv-fintrack/sw.js', { scope: '/ksv-fintrack/' })})}
