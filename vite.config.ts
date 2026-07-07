edae3e637969cacd732c5d95db418512097cd674
phileory <muhammad.luthfi.chani@gmail.com>
2026-07-07 21:45:03 +0700

    Perbaikan base path untuk github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react' //  Benar
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/ksv-fintrack/',
   plugins: [
     react(),
     VitePWA({
